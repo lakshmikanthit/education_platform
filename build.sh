45854f6b0c743a09eb9153272c374da19f99b88e
lakshmikanthit <lakshmikanthtn48@gmail.com>
2025-03-28 22:25:54 +0530

    Added build script for Render deployment

--- build.sh
+++ build.sh
@@ -2,12 +2,11 @@
 # exit on error
 set -o errexit
 
-# Install Python dependencies
-pip install --upgrade pip
+# Install python dependencies
 pip install -r requirements.txt
 
 # Collect static files
 python manage.py collectstatic --no-input
 
-# Run database migrations
+# Run migrations
 python manage.py migrate 
